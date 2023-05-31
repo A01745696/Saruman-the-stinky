9e1489db8b34f240d02a2011719134a04462b483
A01745597 <134191359+FernandoNaC@users.noreply.github.com>
2023-05-30 22:00:39 -0600

    Add names

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+//  Final Project: Quiz Application with Microservices
+//  Date: 30-May-2023
+//  Authors:
+//           A01745597 Fernando Emilio Nava Cano
+//           A01745696 Yahir Cortes Rodriguez
+//           A01025261 Maximiliano Carrasco Rojas
+//           A01742023 Alberto Jashua Rodriguez Villegas
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
